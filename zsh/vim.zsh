baff38ece63af1b007f027b71abaf317b40c253c
Hendra <hendra2392@gmail.com>
2018-02-14 21:30:19 +0700

    Add kill-line binding

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -6,6 +6,7 @@ bindkey '^N' down-history
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
+bindkey '^u' backward-kill-line
 bindkey '^r' history-incremental-search-backward
 
 export KEYTIMEOUT=1
