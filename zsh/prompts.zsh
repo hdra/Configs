1fd4be797437f860d527832817700c81644d8f44
Hendra <hendra2392@gmail.com>
2020-08-28 21:12:56 +0700

    Updates

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -4,6 +4,7 @@ autoload -Uz promptinit && promptinit
 #Prevent pure from doing remote check
 PURE_GIT_PULL=0
 PURE_PROMPT_SYMBOL="❯❯"
+PURE_PROMPT_VICMD_SYMBOL="❮❮"
 PURE_GIT_UP_ARROW="▲"
 PURE_GIT_DOWN_ARROW="▼"
 prompt pure
